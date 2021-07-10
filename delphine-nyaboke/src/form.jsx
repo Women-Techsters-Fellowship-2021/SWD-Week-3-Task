8df660374db1901258acc3ddcc3886effa0ea30f
delphine nyaboke <delphine@sendyit.com>
2021-07-10 23:40:14 +0300

    Submission for delphine nyaboke

---
+++ delphine-nyaboke/src/form.jsx
@@ -0,0 +1,69 @@
+import useContextGetter from '../hooks/useContextGetter';
+import '../styles/form.css';
+
+function Form() {
+	const context = useContextGetter();
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		if (!context.state.title || !context.state.description) {
+			return false;
+		}
+
+		const newItem = {
+			title: context.state.title,
+			description: context.state.description,
+			id: Date.now(),
+		};
+
+		context.dispatch({
+			type: 'ADD_ITEM',
+			payload: newItem,
+		});
+ 
+		context.dispatch({ type: 'RESET_INPUTS' });
+	};
+
+	const setTitle = e => {
+		context.dispatch({
+			type: 'UPDATE_TITLE',
+			payload: e.target.value,
+		});
+	};
+
+	const setDescription = e => {
+		context.dispatch({
+			type: 'UPDATE_DESCRIPTION',
+			payload: e.target.value,
+		});
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					value={context.state.title}
+					onChange={setTitle}
+					type='text'
+					name='title'
+					id='title'
+					placeholder='Title'
+				/>
+				<textarea
+					type='text'
+					value={context.state.description}
+					onChange={setDescription}
+					name='desc'
+					id='desc'
+					placeholder='Description'
+				/>
+				<button class='btn' type='submit'>
+					Add Item
+				</button>
+			</form>
+		</div>
+	);
+}
+
+export default Form;
---
+++ delphine-nyaboke/src/navigationbar.jsx
@@ -0,0 +1,50 @@
+import useContextGetter from '../hooks/useContextGetter';
+import { Link, useLocation } from 'react-router-dom';
+
+function Navbar() {
+	const {
+		dispatch,
+		state: { isUserLoggedIn },
+	} = useContextGetter();
+	const location = useLocation();
+
+	const logout = () => {
+		dispatch({
+			type: 'LOGOUT',
+		});
+	};
+
+	const renderNav = () => {
+		if (!isUserLoggedIn && location.pathname === '/login') {
+			return (
+				<>
+					<Link to='/register'>Register</Link>
+				</>
+			);
+		}
+
+		if (!isUserLoggedIn && location.pathname === '/register') {
+			return (
+				<>
+					<Link to='/login'>Login</Link>
+				</>
+			);
+		}
+
+		if (isUserLoggedIn) {
+			return (
+				<>
+					<Link to='/shopping-list'>My List</Link>
+					<br />
+					<span onClick={logout}>Logout</span>
+				</>
+			);
+		}
+	};
+
+	return <nav>{renderNav()}</nav>;
+}
+
+export default Navbar;
+
+
